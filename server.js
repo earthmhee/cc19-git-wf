0cf96fcd54fa4c8c15e86b59eb53543b77f02f37
Iam-Jacobbbbbbb <teepakorn.snis@gmail.com>
2025-03-10 15:35:23 +0700

    update user

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const authRoute = require("./routes/auth-route");
+const userRoute = require("./routes/user-route");
 const app = express();
 
 app.use("/auth", authRoute);
+app.use("/user", userRoute);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server on port ${port}`));
